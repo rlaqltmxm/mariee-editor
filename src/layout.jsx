799f849ba6e71fa030d455e7c871983f7764f813
mari <mari@jennifersoft.com>
2020-09-13 16:24:37 +0900

    Apply draft editor

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './header';
+import Paper from './paper';
 import './styles/layout.scss';
 
 export default function Layout() {
     return (
         <div className="wrapper">
-
+            <Header />
+            <div className="content">
+                <Paper/>
+            </div>
         </div>
     );
 }
